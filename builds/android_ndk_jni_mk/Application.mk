791f48d4d3c3ee579a8559d044c8170143f7013a
sisong <housisong@hotmail.com>
2023-12-26 15:37:12 +0800

    changed armeabi(arm-v5,v6) builder from clang to gcc, for  fix xxHash unspport Android armeabi+clang;

--- builds/android_ndk_jni_mk/Application.mk
+++ builds/android_ndk_jni_mk/Application.mk
@@ -6,4 +6,4 @@ APP_LDFLAGS += -Wl,--gc-sections
 APP_STL      := stlport_static
 APP_CPPFLAGS := -std=c++11 -fexceptions
 APP_BUILD_SCRIPT := Android.mk
-APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
